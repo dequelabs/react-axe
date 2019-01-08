f605d9de927979e14f980a02e7b8622d189593d3
Stephen Mathieson <me@stephenmathieson.com>
2019-01-08 16:49:07 -0500

    style: setup Prettier and format all code (#62)
    This patch introduces [`Prettier`](https://prettier.io/) for code formatting. This will make contributing to this repository easier, since "code style" is no longer something contributors need to worry about. Prettier is configured to match the project's existing style. However, based on the mixture of tabs/spaces in this repository, I couldn't tell which was preferred. I decided to use spaces because they look nicer when browsing code on GitHub.
    
    All (JS, CSS, JSON, MD) code has been formatted and will be re-formatted every commit (via [`husky`](https://npmjs.org/husky)).
    
    To manually run the formatter, do `npm run fmt`.

--- example/src/globalHeader.js
+++ example/src/globalHeader.js
@@ -2,9 +2,11 @@ import React from 'react';
 import createReactClass from 'create-react-class';
 
 module.exports = createReactClass({
-    render: function() {
-        return <header role="oops">
-            <h1>Our services</h1>
-        </header>;
-    }
+  render: function() {
+    return (
+      <header role="oops">
+        <h1>Our services</h1>
+      </header>
+    );
+  }
 });
